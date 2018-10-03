cd3cda6e45622e9443e2d99169b335b15308e8af
justinrobertson <jusrobertson@gmail.com>
2018-10-03 09:06:50 +0800

    First commit

---
+++ src/httpclientexample/HTTPClientExample.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package httpclientexample;
+
+/**
+ *
+ * @author justin
+ */
+public class HTTPClientExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm form = new MainForm();
+        form.setVisible(true);
+    }
+    
+}
